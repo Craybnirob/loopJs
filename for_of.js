c6e089f2bf14473362785b5880ec49a8d740d21d
Craybnirob <74036538+Craybnirob@users.noreply.github.com>
2021-01-09 06:40:55 -0600

    change

--- for_of.js
+++ for_of.js
@@ -1,21 +1,17 @@
-/*
 var cars, x;
 cars = ['Audi', 'Saab', 'Volvo', 'Mini'];
 
 for (x of cars) {
     document.write(x + '<br>');
 }
-*/
 
 // for of loop array return;
 
-/*
 var txt, x;
 txt = 'Hello JavaScript!';
 
 for (x of txt) {
     document.write(x + '<br>');
 }
-*/
 
 // for of loop txt index 
