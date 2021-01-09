fe403e1389ebad7c80ce30a627d7594288dd0256
Craybnirob <74036538+Craybnirob@users.noreply.github.com>
2021-01-09 06:37:20 -0600

    change commit

--- do_while.js
+++ do_while.js
@@ -1,4 +1,3 @@
-/*
 var txt = '';
 var i = 0;
 
@@ -8,11 +7,9 @@ do {
 } while (i < 10);
 
 document.write(txt);
-*/
 
 // do while number return
 
-/*
 var txt, i;
 txt = '';
 i = 10;
@@ -22,11 +19,9 @@ do {
     i--;
 } while (i > 0);
 document.write(txt);
-*/
 
 // do while number reverse
 
-/*
 var cars, len, txt, i;
 cars = ['Audi', 'Saab', 'Ford', 'Volvo'];
 len = cars.length;
@@ -38,6 +33,5 @@ do {
     i++;
 } while (i < len);
 document.write(txt);
-*/
 
 // do while loop array return
